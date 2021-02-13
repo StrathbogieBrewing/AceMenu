d47c8aa32ca5daa8041d58488ef19c4464a54fa8
Johny <johnyhoops@gmail.com>
2021-02-13 15:54:12 +1100

    revert to original terminal example

--- examples/Terminal/Terminal.ino
+++ examples/Terminal/Terminal.ino
@@ -1,45 +1,32 @@
 #include "AceMenu.h"
 #include "Terminal.h"
 
-#include "variant.h"
-
 #define kTemperatureDelta 3
-#define kEepromStartTime ((uint16_t *)0)
-#define kEepromDuration ((uint16_t *)2)
-#define kEepromEvents ((uint16_t *)4)
+
+#define kEepromSetpoint ((uint16_t *)0)
+#define kEepromHysteresis ((uint16_t *)2)
 
 int temperature = 200;
 int heater = 0;
 
-const int pwm_pin = 9; // ic pin 15
-
 // getter functions
-int getTime() { return rtc_minutes; }
-int getOsc() { return (unsigned int)OSCCAL; }
-int getCal() { return rtc_cal; }
-int getStartTime() { return (int)eeprom_read_word(kEepromStartTime); }
-int getDuration() { return (int)eeprom_read_word(kEepromDuration); }
-int getEvents() { return (int)eeprom_read_word(kEepromEvents); }
-int getSeconds() { return rtc_seconds; }
+int getTemperature() { return (temperature); }
+int getHeater() { return heater; }
+int getSetPoint() { return (int)eeprom_read_word(kEepromSetpoint); }
+int getHysteresis() { return (int)eeprom_read_word(kEepromHysteresis); }
 
-// setters functions
-void setTime(int v) { rtc_minutes = v; }
-void setStartTime(int v) { eeprom_write_word(kEepromStartTime, (uint16_t)v); }
-void setDuration(int v) { eeprom_write_word(kEepromDuration, (uint16_t)v); }
-void setEvents(int v) { eeprom_write_word(kEepromEvents, (uint16_t)v); }
+// setters functions, no setter for the temperature and heater read only
+void setSetPoint(int v) { eeprom_write_word(kEepromSetpoint, (uint16_t)v); }
+void setHysteresis(int v) { eeprom_write_word(kEepromHysteresis, (uint16_t)v); }
 
 // menu contents
 menuItem menuItems[] = {
     // name, getter, setter, format, min value , max value
-    {"CAL", getCal, NULL, Format::unit, 0, 1439},
-    {"CLOCK", getTime, setTime, Format::time, 0, 1439},
-    {"START", getStartTime, setStartTime, Format::time, 0, 1439},
-    {"DURATION", getDuration, setDuration, Format::unit, 1, 59},
-    {"EVENTS", getEvents, setEvents, Format::unit, 1, 24},
-    {"OSC", getOsc, NULL, Format::unit, 0, 1},
-    {"SECS", getSeconds, NULL, Format::unit, 0, 1},
+    {"TEMP", getTemperature, NULL, Format::deci, 0, 0},
+    {"SET", getSetPoint, setSetPoint, Format::deci, 100, 400},
+    {"HYST", getHysteresis, setHysteresis, Format::deci, 5, 100},
+    {"HEAT", getHeater, NULL, Format::boolean, 0, 1},
 };
-
 #define kMenuCount (sizeof(menuItems) / sizeof(menuItem))
 
 Terminal terminal(Serial, 9600);
@@ -49,52 +36,31 @@ AceMenu menu = AceMenu(menuItems, kMenuCount, terminal, terminal);
 void setup() {
   // initialise the menu
   menu.begin();
-
-  // pinMode(pwm_pin, OUTPUT);
-  // analogWrite(pwm_pin, 128);
-  Serial.begin(9600);
 }
 
 void loop() {
-
-  // printf("cal=%d\tosc=%d\tsec=%d\n", rtc_cal, (unsigned int)OSCCAL,
-  // rtc_seconds);
-  Serial.println(rtc_seconds);
-  Serial.println(rtc_cal);
-  Serial.println(OSCCAL);
-  Serial.flush();
-  if (rtc_minutes & 0x01)
-    rtc_sleep();
-  else {
-    unsigned char s = rtc_seconds;
-    while (s == rtc_seconds) {
-    }
-  }
-
-  // if(rtc_sleep() == false){
-  // update the menu if not in sleep state
+  // update the menu
   menu.update();
-  // };      // Enter sleep mode.
 
   // update temperature control
-  // if (heater == true) {
-  //   if (temperature > getStartTime() + getDuration())
-  //     heater = false;
-  // } else {
-  //   if (temperature < getStartTime() - getDuration())
-  //     heater = true;
-  // }
+  if (heater == true) {
+    if (temperature > getSetPoint() + getHysteresis())
+      heater = false;
+  } else {
+    if (temperature < getSetPoint() - getHysteresis())
+      heater = true;
+  }
 
   // for demo make the temperature value change every 500 ms
-  // static int temperatureDelta = kTemperatureDelta;
-  // static long unsigned int waitUntil = 0;
-  // if (millis() > waitUntil) {
-  //   waitUntil = millis() + 500L;
-  //   // ramp up and down
-  //   temperature += temperatureDelta;
-  //   if (temperature > 250)
-  //     temperatureDelta = -kTemperatureDelta;
-  //   if (temperature < 150)
-  //     temperatureDelta = kTemperatureDelta;
-  // }
+  static int temperatureDelta = kTemperatureDelta;
+  static long unsigned int waitUntil = 0;
+  if (millis() > waitUntil) {
+    waitUntil = millis() + 500L;
+    // ramp up and down
+    temperature += temperatureDelta;
+    if (temperature > 250)
+      temperatureDelta = -kTemperatureDelta;
+    if (temperature < 150)
+      temperatureDelta = kTemperatureDelta;
+  }
 }
